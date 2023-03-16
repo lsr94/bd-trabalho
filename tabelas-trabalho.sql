c13f193e767e8f9b42cfc633061bddeebc82efba
lsr94 <110498717+lsr94@users.noreply.github.com>
2023-03-16 10:57:38 -0300

    Add files via upload

---
+++ tabelas-trabalho.sql
@@ -0,0 +1,16 @@
+CREATE TABLE antena (
+	idAntena DECIMAL(3) PRIMARY KEY,
+	idSatelite_Com DECIMAL(3),
+	chaveDescrip DECIMAL(5) NOT NULL, --não sei qual tipo de dado colocar e deve ser not null?
+	
+	CONSTRAINT fk_idSatelite_Com FOREIGN KEY idSatelite_Com REFERENCES SateliteComunicacao
+);
+
+CREATE TABLE modem (
+	idModem DECIMAL(3) PRIMARY KEY,
+	idAntena DECIMAL(3),
+	nomeRede VARCHAR(30) NOT NULL,
+	senhaRede VARCHAR(15) NOT NULL,
+	
+	CONSTRAINT fk_idAntena FOREIGN KEY idAntena REFERENCES antena
+);
