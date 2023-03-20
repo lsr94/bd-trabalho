78e9a0472b029133094fc5688e6cb89e4a2bade6
letmarchezi <leticiabossatto@gmail.com>
2023-03-20 19:25:23 -0300

    Adição SateliteCom e Met

--- tabelas-trabalho.sql
+++ tabelas-trabalho.sql
@@ -5,12 +5,44 @@
 DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
 
+CREATE TABLE SATELITE(
+  idSatelite INTEGER PRIMARY KEY
+  -- falta completar!
+);
+
+CREATE TABLE SATELITECOMUNICACAO (
+  idSatelite INTEGER PRIMARY KEY,
+  chaveCripto CHAR(20) NOT NULL,
+  CONSTRAINT fk_idSateliteC FOREIGN KEY (idSatelite) REFERENCES SATELITE (idSatelite)
+);
+
+Create table SATELITEOBSMET(
+  idSatelite INTEGER PRIMARY KEY,
+  raioLeituraKm DECIMAL(20,5) not null,
+  CONSTRAINT fk_idSateliteO FOREIGN KEY (idSatelite) REFERENCES SATELITE (idSatelite)
+);
+
+create table LEITURA(
+  idSateliteObsM iNTEGER,
+  data_leitura DATE,
+  horario TIME,
+  temperaturaC DECIMAL(5,5),
+  umidade_porc DECIMAL(3,5),
+  imagem bytea,
+  CONSTRAINT fk_idSateliteObsM FOREIGN KEY (idSateliteObsM)
+  	REFERENCES	SateliteObsMet (idSatelite),
+  PRIMARY KEY (idSateliteObsM, data_leitura, horario)
+);
+
+create table USUARIO(
+  idUsuario INTEGER PRIMARY KEY
+);
+  
 CREATE TABLE ANTENA (
 	idAntena INTEGER PRIMARY KEY,
 	idSatelite_Com INTEGER not NULL,
-	chaveDescrip CHAR(20) NOT NULL --não sei qual tipo de dado colocar e deve ser not null?
-	
-	--CONSTRAINT fk_idSatelite_Com FOREIGN KEY (idSatelite_Com) REFERENCES SateliteComunicacao (idSatelite)
+	chaveDescrip CHAR(20) NOT NULL, -- vamos deixar como not null apenas as chaves candidatas?
+	CONSTRAINT fk_idSatelite_Com FOREIGN KEY (idSatelite_Com) REFERENCES SATELITECOMUNICACAO (idSatelite)
 );
 
 CREATE TABLE MODEM (
@@ -21,30 +53,10 @@ CREATE TABLE MODEM (
 	CONSTRAINT fk_idAntena FOREIGN KEY (idAntena) REFERENCES ANTENA (idAntena)
 );
 
-CREATE table USUARIO(
-  idUsuario INTEGER PRIMARY KEY
-);
-
 CREATE TABLE CONECTA(
   idModem INTEGER NOT NULL,
   idUsuario INTEGER NOT NULL,
   CONSTRAINT pk_conecta PRIMARY KEY (idModem, idUsuario),
   CONSTRAINT fk_idModem FOREIGN KEY (idModem) REFERENCES	MODEM (idModem),
   CONSTRAINT fk_idUsuario FOREIGN KEY (idUsuario) references USUARIO (idUsuario)
-);
-
-CREATE table SATELITEOBSMET(
-  idSatelite INTEGER PRIMARY KEY
-);
-
-CREATE table LEITURA(
-  idSateliteObsM iNTEGER,
-  data_leitura DATE,
-  horario TIME,
-  temperaturaC DECIMAL(5,5),
-  umidade_porc DECIMAL(3,5),
-  imagem bytea,
-  CONSTRAINT fk_idSateliteObsM FOREIGN KEY (idSateliteObsM)
-  	REFERENCES	SateliteObsMet (idSatelite),
-  PRIMARY KEY (idSateliteObsM, data_leitura, horario)
 );
