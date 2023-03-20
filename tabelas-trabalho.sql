e4c95f4178c75a075f9f5af0b2779a1c6106bce7
letmarchezi <leticiabossatto@gmail.com>
2023-03-20 19:03:16 -0300

    Correção Create SateliteObsMet

--- tabelas-trabalho.sql
+++ tabelas-trabalho.sql
@@ -1,29 +1,25 @@
 --sugestão de sequência de criação das tabelas para não gerar conflito: central, satélite, satélite comunicação, satélite obs-met, leitura, localização,
 --funcionário, pertence, consulta, usuário, antena, modem, conecta
 
--- Apagar todas as tabelas e recriar o schema: descomente as 2 linahs abaixo
---DROP SCHEMA public CASCADE;
---CREATE SCHEMA public;
-
-CREATE TABLE ANTENA (
+CREATE TABLE antena (
 	idAntena INTEGER PRIMARY KEY,
 	idSatelite_Com INTEGER not NULL,
-	chaveDescrip CHAR(20) NOT NULL --não sei qual tipo de dado colocar e deve ser not null?
+	chaveDescrip CHAR(20) NOT NULL, --não sei qual tipo de dado colocar e deve ser not null?
 	
-	--CONSTRAINT fk_idSatelite_Com FOREIGN KEY (idSatelite_Com) REFERENCES SateliteComunicacao (idSatelite)
+	CONSTRAINT fk_idSatelite_Com FOREIGN KEY (idSatelite_Com) REFERENCES SateliteComunicacao (idSatelite)
 );
 
-CREATE TABLE MODEM (
+CREATE TABLE modem (
 	idModem INTEGER PRIMARY KEY,
 	idAntena INTEGER NOT NULL,
 	nomeRede VARCHAR(30) NOT NULL,
 	senhaRede VARCHAR(15) NOT NULL,
-	CONSTRAINT fk_idAntena FOREIGN KEY (idAntena) REFERENCES ANTENA (idAntena)
+	CONSTRAINT fk_idAntena FOREIGN KEY (idAntena) REFERENCES antena (idAntena)
 );
 
 create table USUARIO(
   idUsuario INTEGER PRIMARY KEY
-);
+)
 
 CREATE TABLE CONECTA(
   idModem INTEGER NOT NULL,
@@ -31,12 +27,10 @@ CREATE TABLE CONECTA(
   CONSTRAINT pk_conecta PRIMARY KEY (idModem, idUsuario),
   CONSTRAINT fk_idModem FOREIGN KEY (idModem) REFERENCES	MODEM (idModem),
   CONSTRAINT fk_idUsuario FOREIGN KEY (idUsuario) references USUARIO (idUsuario)
-);
-
-table SATELITEOBSMET(
+)
+create table SATELITEOBSMET(
   idSatelite INTEGER PRIMARY KEY
-);
-
+)
 create table LEITURA(
   idSateliteObsM iNTEGER,
   data_leitura DATE,
@@ -47,4 +41,4 @@ create table LEITURA(
   CONSTRAINT fk_idSateliteObsM FOREIGN KEY (idSateliteObsM)
   	REFERENCES	SateliteObsMet (idSatelite),
   PRIMARY KEY (idSateliteObsM, data_leitura, horario)
-);
+)
