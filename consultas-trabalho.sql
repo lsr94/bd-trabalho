3d5ae7be245aa830f44d6519f840b33bdabdc611
Leticia Marchezi <39420862+letMarchezi@users.noreply.github.com>
2023-03-22 15:36:05 -0300

    Update consultas-trabalho.sql

--- consultas-trabalho.sql
+++ consultas-trabalho.sql
@@ -1,4 +1,7 @@
---CONSULTA SIMPLES
+--///////////////////////////////////////////////////////////////////////////
+-- CONSULTA SIMPLES
+--///////////////////////////////////////////////////////////////////////////
+-- Solicitadas pelos clientes --
 -- 1. Encontre todos os satélites que estão conectados a uma central ‘x’. Vamos considerar que a central 'x' está localizada na cidade de São Carlos.
 SELECT * FROM CENTRAL WHERE cidade = 'São Carlos';
 
@@ -12,6 +15,12 @@ SELECT * FROM SATELITE WHERE datalancamento < '01-01-2010';
 SELECT chaveCripto FROM SATELITECOMUNICACAO;
 
 -- 5. Encontre todos os funcionários da central ‘x’. Vamos considerar que a central 'x' está localizada na cidade de São Carlos.
+SELECT f.* FROM funcionario f, central ce, pertence pe
+where f.idfuncionario = pe.idfuncionario AND 
+	pe.idcentral = ce.idcentral AND 
+    ce.cidade = 'São Carlos';
+    
+-- Criadas pelo grupo --
 
 -- 6. 
 
@@ -23,15 +32,26 @@ SELECT chaveCripto FROM SATELITECOMUNICACAO;
 
 -- 10.
 
---CONSULTAS COMPLEXAS
+--///////////////////////////////////////////////////////////////////////////
+-- CONSULTAS COMPLEXAS
+--///////////////////////////////////////////////////////////////////////////
 -- 1. Encontre o nome e o número de identificação de todos os satélites de observação que foram lançados nos últimos 5 anos.
 SELECT sateliteobsmet.idsatelite, satelite.nome FROM SATELITE, SATELITEOBSMET WHERE satelite.datalancamento BETWEEN '03-22-2018' AND '03-22-2023' AND 
 satelite.idsatelite = sateliteobsmet.idsatelite;
 
 -- 2. Encontre o nome e o número de identificação de todos os satélites de comunicação que possuem uma velocidade de DownLink maior que 100 Mbps.
-
+SELECT s.idsatelite, s.nome from satelite s, satelitecomunicacao sc, antena an, modem mo, conecta co, usuario u
+	WHERE s.idsatelite = sc.idsatelite AND
+    sc.idsatelite = an.idsatelite_com AND
+    an.idantena = mo.idantena AND
+    co.idmodem = mo.idmodem	AND
+    co.idusuario = u.idusuario AND
+    u.velocidadedownlink > 100;
+    
 
 -- 3. Encontre o nome e a posição espacial de todos os satélites de observação que registraram uma temperatura acima de 30 graus Celsius nas últimas 24 horas.
+
+-- Criadas pelo grupo: --
 
 -- 4.
 
